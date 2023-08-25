4c3a2a6cfb37344247df33eb948c83ee3f0ab6ad
Rajababu Swain <rajababu.swain@prorigosoftware.com>
2023-08-25 18:06:41 +0530

    made changes in palindrome

--- com.prorigo.trainning/src/Assignment1/palindrome.java
+++ com.prorigo.trainning/src/Assignment1/palindrome.java
@@ -13,7 +13,7 @@ public class palindrome {
 		for (int i = length - 1; i >= 0; i--)
 			reverse = reverse + original.charAt(i);
 		if (original.equals(reverse))
-			System.out.println("Entered string/number is a palindrome");
+			System.out.println("Entered string/number is a palindrome");//ahjsufvsh
 		else
 			System.out.println("Entered string/number isn't a palindrome");
 		in.close();
